070c982099270926e8eb2729f0b3e3387bbf60d5
marcelo <MRolqueza@gmail.com>
2025-06-14 17:22:04 -0400

    Estructura Proyecto 1 - Testeado

--- src/components/layout/partials/NavMenuDrawer.vue
+++ src/components/layout/partials/NavMenuDrawer.vue
@@ -1,60 +1,88 @@
 <!-- NavMenu.vue -->
 <template>
   <v-list density="compact" nav>
-    <v-list-item prepend-icon="mdi-home" title="Inicio" value="home" to="/"></v-list-item>
+    <v-list-item prepend-icon="mdi-home-outline" title="Inicio" value="home" to="/"></v-list-item>
 
-    <v-list-group value="Estructuracion">
+    <v-list-group value="estructuracion">
       <template v-slot:activator="{ props }">
-        <v-list-item
-          v-bind="props"
-          prepend-icon="mdi-chart-timeline-variant"
-          title="Estructuracion"
-        ></v-list-item>
+        <v-list-item v-bind="props" prepend-icon="mdi-domain" title="Estructuración"></v-list-item>
       </template>
 
       <v-list-item
         prepend-icon="mdi-notebook-outline"
         title="PEI"
-        value="pei"
+        value="pei-list"
         to="/pei"
       ></v-list-item>
 
       <v-list-item
-        prepend-icon="mdi-library-shelves"
+        prepend-icon="mdi-folder-table-outline"
         title="Proyectos"
-        value="projects"
+        value="projects-list"
         to="/proyecto"
       ></v-list-item>
     </v-list-group>
 
-    <v-list-group value="Planning">
+    <v-list-group value="planning">
       <template v-slot:activator="{ props }">
         <v-list-item
           v-bind="props"
-          prepend-icon="mdi-calendar-multiple"
-          title="Planificacion"
+          prepend-icon="mdi-calendar-edit"
+          title="Planificación"
         ></v-list-item>
       </template>
 
       <v-list-item
-        prepend-icon="mdi-calendar-check"
+        prepend-icon="mdi-playlist-edit"
         title="Proyectos/Actividades"
-        value="proyectosActividades"
+        value="planning-activities"
         to="/planificacion/actividades"
       ></v-list-item>
 
       <v-list-item
-        prepend-icon="mdi-chart-gantt"
+        prepend-icon="mdi-timeline-outline"
         title="Cronogramas"
-        value="schedules"
+        value="planning-schedules"
         to="/planificacion/cronogramas"
       ></v-list-item>
     </v-list-group>
 
-    <v-list-item prepend-icon="mdi-help-circle" title="Ayuda" value="help" to="/help"></v-list-item>
+    <v-list-group value="monitoreo">
+      <template v-slot:activator="{ props }">
+        <v-list-item v-bind="props" prepend-icon="mdi-eye-outline" title="Monitoreo"></v-list-item>
+      </template>
+
+      <v-list-item
+        prepend-icon="mdi-account-alert-outline"
+        title="Solicitud/Reposición"
+        value="monitoring-requests"
+        to=""
+      ></v-list-item>
+
+      <v-list-item
+        prepend-icon="mdi-chart-bar"
+        title="Rendición Cuentas"
+        value="monitoring-reports"
+        to=""
+      ></v-list-item>
+
+      <v-list-item
+        prepend-icon="mdi-file-document-outline"
+        title="Informe de Actividad"
+        value="monitoring-activity"
+        to=""
+      ></v-list-item>
+    </v-list-group>
 
     <v-list-item
-      prepend-icon="mdi-information"
+      prepend-icon="mdi-help-circle-outline"
+      title="Ayuda"
+      value="help"
+      to="/help"
+    ></v-list-item>
+
+    <v-list-item
+      prepend-icon="mdi-information-outline"
       title="Acerca de"
       value="about"
       to="/about"
@@ -63,3 +91,18 @@
 </template>
 
 <script setup></script>
+<style scoped>
+.v-list-item {
+  transition: all 0.3s ease;
+}
+
+.v-list-item:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+/* Estilo para el ítem activo */
+.v-list-item--active {
+  background-color: rgba(25, 118, 210, 0.1);
+  border-left: 3px solid #1976d2;
+}
+</style>
