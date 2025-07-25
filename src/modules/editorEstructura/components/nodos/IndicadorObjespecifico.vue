1f42028f2e0a64b97a8877b7c87dd8109522e4aa
marcelo <MRolqueza@gmail.com>
2025-07-25 18:27:33 -0400

    Nodo de oe og, campos descripcion y definicion agregados

--- src/modules/editorEstructura/components/nodos/IndicadorObjespecifico.vue
+++ src/modules/editorEstructura/components/nodos/IndicadorObjespecifico.vue
@@ -4,8 +4,8 @@
     <template #default>
       <div class="text-body-2">
         <div class="mb-1"><strong>Código:</strong> {{ data.nodoProyecto.codigo }}</div>
-        <div class="mb1"><strong>Descripcion:</strong> {{ data }}</div>
-        <div class="mb1"><strong>Definicion:</strong> {{ data }}</div>
+        <div class="mb1"><strong>Descripcion:</strong> {{ data.nodoProyecto.descripcion }}</div>
+        <div class="mb1"><strong>Definicion:</strong> {{ data.nodoProyecto.definicion }}</div>
         <div class="mb-1"><strong>Redaccion:</strong> {{ data.nodoProyecto.redaccion }}</div>
         <div class="mb-1">
           <strong>Fuente de verificacion:</strong> {{ data.nodoProyecto.fuente_verificacion }}
@@ -18,7 +18,6 @@
         <div class="mb-1"><strong>target q1</strong> {{ data.nodoProyecto.target_q1 }}</div>
         <div class="mb-1"><strong>target q2</strong> {{ data.nodoProyecto.target_q2 }}</div>
         <div class="mb-1"><strong>target q3</strong> {{ data.nodoProyecto.target_q3 }}</div>
-        <div class="mb-1"><strong>target q4</strong> {{ data.nodoProyecto.target_q4 }}</div>
       </div>
     </template>
 
