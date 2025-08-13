2609eda9310d5988068248a7c58fd2322b43b2c0
marcelo <MRolqueza@gmail.com>
2025-08-13 15:51:47 -0400

    Usuario, servicio y composable modificado

--- src/modules/usuarios/services/usuariosService.js
+++ src/modules/usuarios/services/usuariosService.js
@@ -1,4 +1,5 @@
 import { apiUsuarios } from '@/services/axios'
+import api from '@/services/axios'
 
 export const usuarioServicios = {
   usuarioDatosPorNick: async (usuarioNick) => {
@@ -7,6 +8,34 @@ export const usuarioServicios = {
       return respuesta.data
     } catch (error) {
       console.error('Axios: error datos del usuario: ' + usuarioNick, error)
+      throw error
+    }
+  },
+  /* Usuario por ID */
+  usuarioPorId: async (id) => {
+    try {
+      const respuesta = await api.get('/usr/usuario/' + id + '/')
+      return respuesta.data
+    } catch (error) {
+      console.error('Axios: error datos del usuario: ' + id, error)
+      throw error
+    }
+  },
+  usuariosTodos: async () => {
+    try {
+      const respuesta = await api.get('/usr/usuarios/')
+      return respuesta.data
+    } catch (error) {
+      console.error('Axios: error datos del usuario: ', error)
+      throw error
+    }
+  },
+  regUsuario: async (usrData) => {
+    try {
+      const respuesta = await api.post('/usr/registrar/', usrData)
+      return respuesta.data
+    } catch (error) {
+      console.error('Axios: error al registrar el usuario ', error)
       throw error
     }
   },
